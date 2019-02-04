b7b2b2e46b1f373e5409a298c9549f7bc6d908e7
Nenad <nstokic@gmail.com>
2019-02-04 19:44:51 +0100

    Challenge finished lesson 280

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -18,7 +19,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("home", {homePContent: homeStartingContent, posts:posts});
+  res.render("home", {homePContent: homeStartingContent, posts: posts});
 });
 
 app.get("/about", function(req, res){
@@ -34,10 +35,17 @@ app.get("/compose", function(req, res){
 });
 
 app.get("/posts/:posttitle", function(req, res){
+  const titleFromParam = _.lowerCase(req.params.posttitle);
+
   posts.forEach(function(post){
-    if(post.title === req.params.posttitle) {
-      console.log("Match found");
+
+    const postTitle = _.lowerCase(post.title);
+    const postContent = post.content;
+
+    if(postTitle === titleFromParam) {
+      res.render("post", {title: post.title, content: post.content});
     }
+
   });
 });
 
